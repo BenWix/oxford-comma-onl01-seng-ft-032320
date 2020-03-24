b5f2d058c852171f3d45c3b7b7fc0c06f48b5e7b
Benjamin Wichser <benwichser@gmail.com>
2020-03-24 14:52:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,9 @@
 def oxford_comma(array)
-  array[-1] = "and " + array[-1]
-  array.join(", ")
+  if array.length >= 2
+    array[-1] = "and " + array[-1]
+    array.length > 2 ? array.join(", ") : array.join(" ")
+  else 
+    array
+  end 
+  
 end
